d92d050bc1fd5e18057da7c899c1bcec5d4a72e2
Noospaceio <helferhotline@proton.me>
2025-09-06 16:26:02 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
-const Noospace = dynamic(() => import("../components/Noospace"), { ssr: false });
+const Noospace = dynamic(() => import('../components/Noospace'), { ssr: false });
 
 export default function Home() {
   const router = useRouter();
   const mode = router.query.mode;
 
-  return <Noospace guestMode={mode === "guest"} />;
+  return <Noospace guestMode={mode === 'guest'} />;
 }
-
